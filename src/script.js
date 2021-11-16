aca765a8e5adddd591cb8191a8124fbfc922bdde
hnicolus <nicolusmaluleke@gmail.com>
2021-11-16 05:19:07 +0200

    Added Orbit controls

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
 /**
  * Object
  */
@@ -19,16 +18,34 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: innerWidth,
+    height: innerHeight
 }
 
+window.addEventListener('resize', (event) => {
+
+    sizes.height = innerHeight;
+    sizes.width = innerWidth;
+
+    //update camera aspect
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
@@ -37,4 +54,11 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+const tick = () => {
+
+    controls.update()
+    renderer.render(scene, camera)
+    requestAnimationFrame(tick)
+}
+tick()
